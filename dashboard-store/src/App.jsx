63aa06234a0d9d8c2d0cfa732e22a5c4fbd1f8a7
Juan Estepa <juan.gonzalez36@uptc.edu.co>
2023-12-12 15:46:12 -0500

    Tercer Commit

--- dashboard-store/src/App.jsx
+++ dashboard-store/src/App.jsx
@@ -1,12 +1,42 @@
+import { useState } from "react";
+import {
+  RiMenu3Fill,
+  RiUser3Line,
+  RiAddLine,
+  RiPieChartLine,
+  RiCloseLine,
+} from "react-icons/ri";
+
 import Sidebar from "./components/shared/Sidebar";
 
 function App() {
+  const [showMenu, setShowMenu] = useState(false);
+  const [showOrder, setShowOrder] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
 
   return (
     <div className="bg-[#262837] w-full min-h-screen">
-      <Sidebar />
+      <Sidebar showMenu={showMenu} />
+      {/*menú movil*/}
+      <nav className="bg-red-[#1F1D2B] lg:hidden fixed w-full bottom-0 left-0 text-3xl rounded-tl-xl rounded-tr-xl text-gray-400 py-2 px-8 flex items-center justify-between">
+        <button className="p-2">
+          <RiUser3Line />
+        </button>
+        <button className="p-2">
+          <RiAddLine />
+        </button>
+        <button className="p-2">
+          <RiPieChartLine />
+        </button>
+        <button onClick={toggleMenu} className="text-white p-2">
+          {showMenu ? <RiCloseLine /> : <RiMenu3Fill />}
+        </button>
+      </nav>
     </div>
-  )
+  );
 }
 
 export default App;
