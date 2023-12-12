960642e5f114a5527218c826b5782d6435568e4d
Miguel Angel Alfonso <105066751+MiguelAlfonso1723@users.noreply.github.com>
2023-12-11 21:33:47 -0500

    Segundo Commit

--- dashboard-store/src/App.jsx
+++ dashboard-store/src/App.jsx
@@ -1,11 +1,12 @@
+import Sidebar from "./components/shared/Sidebar";
 
 function App() {
 
   return (
-    <div className="App">
-      Hola mundo
+    <div className="bg-[#262837] w-full min-h-screen">
+      <Sidebar />
     </div>
   )
 }
 
-export default App
+export default App;
